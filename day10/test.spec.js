c98b112d4dec72c969891fde5f87826995c822e9
Charles Parmentier <charles.parmentier@hotmail.com>
2020-12-21 15:06:12 +0100

    Tenth Day (erasing useless code)

--- day10/test.spec.js
+++ day10/test.spec.js
@@ -103,31 +103,4 @@ describe("Day 10", () => {
         it("Should return undefined", () => expect(secondFunc()).to.be.undefined)
         it("Should return null", () => expect(secondFunc([])).to.be.null)
     })
-//     describe("Part 2", () => {
-//         it("Should return 62", () => expect(secondFunc([
-// 35,
-// 20,
-// 15,
-// 25,
-// 47,
-// 40,
-// 62,
-// 55,
-// 65,
-// 95,
-// 102,
-// 117,
-// 150,
-// 182,
-// 127,
-// 219,
-// 299,
-// 277,
-// 309,
-// 576
-//         ], 5)).to.equal(62))
-//         it("Should return undefined", () => expect(secondFunc()).to.be.undefined)
-//         it("Should return null", () => expect(secondFunc([])).to.be.null)
-//     })
-
 })
